ed19d244366d6ac52cb825bba3e0712ea8312e07
kunal <kunaldaphnis@gmail.com>
2018-11-19 22:00:55 +0530

    Final updates

--- app/src/main/java/com/vijayjaidewan01vivekrai/dynamic_app/Okhttpclient/RetrofitClient.java
+++ app/src/main/java/com/vijayjaidewan01vivekrai/dynamic_app/Okhttpclient/RetrofitClient.java
@@ -1,17 +1,8 @@
 package com.vijayjaidewan01vivekrai.dynamic_app.Okhttpclient;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.support.v7.app.AppCompatActivity;
-
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
-import okhttp3.ResponseBody;
-import okhttp3.logging.HttpLoggingInterceptor;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
